9384831f184aeb389890d4dc689e0e6e3f5f79ee
Fehua1337 <161331310+Fehua1337@users.noreply.github.com>
2024-05-13 19:18:50 +0500

    first commit

---
+++ AIGameProj1/AIGameProj1App.swift
@@ -0,0 +1,17 @@
+//
+//  AIGameProj1App.swift
+//  AIGameProj1
+//
+//  Created by Alisher Tulembekov on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AIGameProj1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
